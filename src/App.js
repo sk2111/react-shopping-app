ec89db94e2ea6c4029ec42b5b34aaca6be4aa846
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-11 15:16:06 +0530

    Add : Collection itelm and view
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Route, Switch } from 'react-router-dom';
+import shopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/shop" component={shopPage}></Route>
       </Switch>
     </div>
   );
