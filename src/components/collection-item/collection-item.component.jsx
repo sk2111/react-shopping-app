cc399775aa13217ce8ad0385b520177a46243b75
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-13 18:24:45 +0530

    Add : Hover effect to additem
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './collection-item.style.scss';
+import CustomButton from '../custom-button/custom-button.component';
 
 const CollectionItem = ({id,name,imageUrl,price})=>{
     return(
         <div className="collection-item">
-        <div className="image" style={{backgroundImage:`url(${imageUrl})`}}></div>
-        <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">${price}</span>
-        </div>
+            <div className="image" style={{backgroundImage:`url(${imageUrl})`}}></div>
+            <div className="collection-footer">
+                <span className="name">{name}</span>
+                <span className="price">${price}</span>
+            </div>
+            <CustomButton inverted>Add to Cart</CustomButton>
         </div>
     )
 };
