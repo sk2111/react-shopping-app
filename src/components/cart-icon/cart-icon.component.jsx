fe11684e56897122edf7b5eaf03397b7072b7048
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-14 12:02:16 +0530

    Add : Memomisation for icon number display
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import {ReactComponent as ShoppingIcon} from '../../assests/shopping-bag.svg';
+import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg';
 import './cart-icon.styles.scss';
-
-const CartIcon = ({...props})=>{
-    return(
-        <div className="cart-icon" {...props}>
-            <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { connect } from 'react-redux';
+const CartIcon = ({ itemCount,...props }) => {
+    return (
+        <div className="cart-icon" onClick={props.onClick}>
+            <ShoppingIcon className="shopping-icon" />
+            <span className="item-count">{itemCount}</span>
         </div>
     );
 };
 
-export default CartIcon;
+const mapStateToProps = (state) => {
+    return {
+        itemCount: selectCartItemsCount(state)
+    }
+}
+export default connect(mapStateToProps)(CartIcon);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,14 @@
+import {createSelector} from 'reselect';
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector([selectCart],(cart)=>{
+    return cart.cartItems
+});
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => {
+        return cartItems.reduce((accQuantity,item)=>accQuantity+item.quantity,0)
+    }
+);
