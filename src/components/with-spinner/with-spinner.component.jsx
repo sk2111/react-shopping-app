94537de03adafd898cefbbbfa1e0aa40425ea943
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-18 15:03:17 +0530

    Loading spinner style fix
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import './with-spinner.styles.scss';
 const WithSpinner = WrappedComponent => ({isLoading,...otherProps})=>{
     console.log("HOC rendering pahse",isLoading);
     return isLoading ? (
-        <div>Loading</div>
+        <div className="loading-spinner-outer-con">
+            <div className="loading-spinner-inner-con"></div>
+        </div>
     ):(
         <WrappedComponent {...otherProps}></WrappedComponent>
     )
