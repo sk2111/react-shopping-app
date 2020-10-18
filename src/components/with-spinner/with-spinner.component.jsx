327c3e9bfbcf97636712e6e6d277218fb440e84c
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-18 15:48:05 +0530

    Fix : Console log fix
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './with-spinner.styles.scss';
 const WithSpinner = WrappedComponent => ({isLoading,...otherProps})=>{
-    console.log("HOC rendering pahse",isLoading);
     return isLoading ? (
         <div className="loading-spinner-outer-con">
             <div className="loading-spinner-inner-con"></div>
