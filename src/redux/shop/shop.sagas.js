4dbf3145ad6ff0dcd63dd605547f9f700a5861c0
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-21 10:14:35 +0530

    Add : Root saga
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import {takeEvery,call,put} from 'redux-saga/effects';
+import {call,put, takeLatest} from 'redux-saga/effects';
 
 import ShopActionTypes from './shop.types';
 import {firestore,convertCollectionSnapShotToMap} from '../../firebase/firebase.utils';
@@ -17,5 +17,5 @@ export function* fetchCollectionsAsync(){
 }
 
 export function* fetchCollectionsStart(){
-    yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START,fetchCollectionsAsync)
+    yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START,fetchCollectionsAsync)
 }
