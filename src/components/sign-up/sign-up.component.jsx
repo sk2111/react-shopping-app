82b476842ebf220ed279c9085f4cb94aa0e28d0a
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-22 15:01:52 +0530

    Refactor : sign up component into hooks
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,53 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import './sign-up.styles.scss';
 import { signUpStart } from '../../redux/user/user.actions';
-import {connect} from 'react-redux';
-class SignUp extends React.Component{
-    constructor(){
-        super();
-        this.state={
-            displayName:'',
-            email:'',
-            password:'',
-            confirmPassword:''
-        }
-    }
-    handleSubmit= async(event)=>{
+import { connect } from 'react-redux';
+
+const SignUp = ({ signUpStart }) => {
+    const [signUpInfo, setSignUpInfo] = useState({ displayName: '', email: '', password: '', confirmPassword: '' });
+
+    const { displayName, email, password, confirmPassword } = signUpInfo;
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        const {displayName,email,password,confirmPassword} = this.state;
-        const {signUpStart} = this.props;
-        if(password !== confirmPassword)
-        {
+        if (password !== confirmPassword) {
             alert("Passwords dont match");
             return;
         }
-        signUpStart({displayName,email,password});
+        signUpStart({ displayName, email, password });
     }
-    handleChange = event=>{
-        const {name,value} = event.target;
-        this.setState({[name]:value});
+    const handleChange = event => {
+        const { name, value } = event.target;
+        setSignUpInfo({ ...signUpInfo, [name]: value });
     }
-    render(){
-        const {displayName,email,password,confirmPassword} = this.state;
-        return(
-            <div className="sign-up">
-                <h2 className="title">I do not have a account</h2>
-                <span className="title">Sign up with your email and password</span>
-                <form className="sign-up-form" onSubmit={this.handleSubmit}>
-                    <FormInput type="text" name="displayName" value={displayName} onChange={this.handleChange} label="Name" required />
-                    <FormInput type="email" name="email" value={email} onChange={this.handleChange} label="Email" required />
-                    <FormInput type="password" name="password" value={password} onChange={this.handleChange} label="Password" required />
-                    <FormInput type="password" name="confirmPassword" value={confirmPassword} onChange={this.handleChange} label="confirm Password" required />         
-                    <CustomButton type="submit">Sign Up</CustomButton>
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div className="sign-up">
+            <h2 className="title">I do not have a account</h2>
+            <span className="title">Sign up with your email and password</span>
+            <form className="sign-up-form" onSubmit={handleSubmit}>
+                <FormInput type="text" name="displayName" value={displayName} onChange={handleChange} label="Name" required />
+                <FormInput type="email" name="email" value={email} onChange={handleChange} label="Email" required />
+                <FormInput type="password" name="password" value={password} onChange={handleChange} label="Password" required />
+                <FormInput type="password" name="confirmPassword" value={confirmPassword} onChange={handleChange} label="confirm Password" required />
+                <CustomButton type="submit">Sign Up</CustomButton>
+            </form>
+        </div>
+    )
 };
-const mapDispatchToProps = (dispatch)=>({
-    signUpStart:(signUpData)=>dispatch(signUpStart(signUpData))
+const mapDispatchToProps = (dispatch) => ({
+    signUpStart: (signUpData) => dispatch(signUpStart(signUpData))
 })
-export default connect(null,mapDispatchToProps)(SignUp);
+export default connect(null, mapDispatchToProps)(SignUp);
