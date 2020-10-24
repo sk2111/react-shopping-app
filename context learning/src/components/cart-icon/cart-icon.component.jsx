8327551d1272314ba25576885d2c1bacb90283c0
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-24 10:57:40 +0530

    Add : cart context hidden
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- context learning/src/components/cart-icon/cart-icon.component.jsx
+++ context learning/src/components/cart-icon/cart-icon.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -8,13 +8,17 @@ import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
+import CartContext from '../../contexts/cart/cart.context';
 
-const CartIcon = ({ toggleCartHidden, itemCount }) => (
-  <div className='cart-icon' onClick={toggleCartHidden}>
-    <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>{itemCount}</span>
-  </div>
-);
+const CartIcon = ({itemCount }) => {
+  const {toggleHidden} = useContext(CartContext)
+  return (
+    <div className='cart-icon' onClick={toggleHidden}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{itemCount}</span>
+    </div>
+  )
+};
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
--- context learning/src/components/header/header.component.jsx
+++ context learning/src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react';
+import React,{useContext,useState} from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -11,10 +11,14 @@ import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import CurrentUserContext from '../../contexts/current-user/current-user.context.js';
+import CartContext from '../../contexts/cart/cart.context';
+
 import './header.styles.scss';
 
-const Header = ({ hidden }) => {
+const Header = () => {
   const currentUser = useContext(CurrentUserContext);
+  const [hidden,setHidden] = useState(true);
+  const toggleHidden = ()=>setHidden(!hidden);
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -36,7 +40,9 @@ const Header = ({ hidden }) => {
               SIGN IN
             </Link>
           )}
-        <CartIcon />
+          <CartContext.Provider value={{hidden,toggleHidden}}>
+            <CartIcon />
+          </CartContext.Provider>
       </div>
       {hidden ? null : <CartDropdown />}
     </div>
