d55fde6e0d830a8b177095ca3045822a9346566b
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-24 10:08:40 +0530

    Add : Initial commit for context learning
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

---
+++ context learning/src/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyAp73MP4kflG0Ynexsl7-_UtVZ53RCpiG4",
+  authDomain: "crwn-db-3d17e.firebaseapp.com",
+  databaseURL: "https://crwn-db-3d17e.firebaseio.com",
+  projectId: "crwn-db-3d17e",
+  storageBucket: "crwn-db-3d17e.appspot.com",
+  messagingSenderId: "116176247314",
+  appId: "1:116176247314:web:e0eb9398237dda4f4016cb",
+  measurementId: "G-02QY45LFH1"
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
