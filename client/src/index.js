dc7de04a961f3fd01b9a8c317dd6d18c3ec99499
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-27 15:34:33 +0530

    Add : Service worker update
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,8 @@ import App from './App';
 import {Provider} from 'react-redux';
 import {store,persistor} from './redux/store';
 
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
@@ -19,3 +21,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
--- server.js
+++ server.js
@@ -23,6 +23,9 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.join(__dirname,'client/build','index.html'))
     })
 }
+app.get('/service-worker.js',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','service-worker.js'));
+});
 
 app.post('/payment',(req,res)=>{
     const body = {
